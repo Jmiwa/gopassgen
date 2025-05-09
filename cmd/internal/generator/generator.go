e4952d6e5b779947a2cb0234432c1abce54064aa
mj <kawaikenmj@gmail.com>
2025-05-09 11:32:32 +0900

    generatorパッケージを作成し、パスワード生成機能を追加

--- cmd/internal/generator/generator.go
+++ cmd/internal/generator/generator.go
@@ -1,1 +1,22 @@
+// internal/generator/generator.go
+package generator
+
+import (
+    "crypto/rand"
+    "math/big"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func Generate(length int) (string, error) {
+    password := make([]byte, length)
+    for i := range password {
+        n, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+        if err != nil {
+            return "", err
+        }
+        password[i] = charset[n.Int64()]
+    }
+    return string(password), nil
+}
 
