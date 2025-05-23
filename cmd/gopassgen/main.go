d1121be38017a14771551154b33801ae2af6739e
mj <kawaikenmj@gmail.com>
2025-05-23 11:45:26 +0900

    CLIフラグを追加し、パスワード生成の引数を動的に設定できるように変更

--- cmd/gopassgen/main.go
+++ cmd/gopassgen/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+    "flag"
     "fmt"
     "github.com/Jmiwa/gopassgen/internal/generator"
 )
 
 func main() {
-    password, err := generator.Generate(12)
+    // CLIフラグの定義
+    length := flag.Int("length", 12, "パスワードの長さ")
+    useDigits := flag.Bool("digits", false, "数字を含める")
+    useSymbols := flag.Bool("symbols", false, "記号を含める")
+
+    flag.Parse() // フラグを解析
+
+    // パスワード生成
+    password, err := generator.Generate(*length, *useDigits, *useSymbols)
     if err != nil {
         panic(err)
     }
     fmt.Println(password)
 }
-
