c1190e83e37dfed32e6efa395d19cd1067626d95
mj <kawaikenmj@gmail.com>
2025-05-23 11:42:41 +0900

    Refactor code structure for improved readability and maintainability

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -6,9 +6,26 @@ import (
     "math/big"
 )
 
-const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const (
+    lower       = "abcdefghijklmnopqrstuvwxyz"
+    upper       = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    digits      = "0123456789"
+    symbols     = "!@#$%^&*()-_=+[]{}<>?/|"
+)
 
-func Generate(length int) (string, error) {
+func Generate(length int, useDigits bool, useSymbols bool) (string, error) {
+    charset := lower + upper
+    if useDigits {
+        charset += digits
+    }
+    if useSymbols {
+        charset += symbols
+    }
+
+    if len(charset) == 0 {
+        return "", nil
+    }
+
     password := make([]byte, length)
     for i := range password {
         n, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
@@ -19,4 +36,3 @@ func Generate(length int) (string, error) {
     }
     return string(password), nil
 }
-
