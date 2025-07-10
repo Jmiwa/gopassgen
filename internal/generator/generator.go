09f350660e9cb468d34d3be7440359319b6eb353
mj <kawaikenmj@gmail.com>
2025-07-10 18:40:21 +0900

    feat: 強制的に記号・数字を含める仕様の追加（v0.1.5）

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -1,19 +1,51 @@
-// internal/generator/generator.go
 package generator
 
 import (
     "crypto/rand"
+    "errors"
     "math/big"
 )
 
 const (
-    lower       = "abcdefghijklmnopqrstuvwxyz"
-    upper       = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    digits      = "0123456789"
-    symbols     = "!@#$%^&*()-_=+[]{}<>?/|"
+    lower   = "abcdefghijklmnopqrstuvwxyz"
+    upper   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    digits  = "0123456789"
+    symbols = "!@#$%^&*()-_=+[]{}<>?/|"
 )
 
+func randomCharFrom(set string) (byte, error) {
+    n, err := rand.Int(rand.Reader, big.NewInt(int64(len(set))))
+    if err != nil {
+        return 0, err
+    }
+    return set[n.Int64()], nil
+}
+
+func shuffle(bytes []byte) ([]byte, error) {
+    for i := len(bytes) - 1; i > 0; i-- {
+        jInt, err := rand.Int(rand.Reader, big.NewInt(int64(i+1)))
+        if err != nil {
+            return nil, err
+        }
+        j := int(jInt.Int64())
+        bytes[i], bytes[j] = bytes[j], bytes[i]
+    }
+    return bytes, nil
+}
+
 func Generate(length int, useDigits bool, useSymbols bool) (string, error) {
+    requiredSets := []string{lower + upper}
+    if useDigits {
+        requiredSets = append(requiredSets, digits)
+    }
+    if useSymbols {
+        requiredSets = append(requiredSets, symbols)
+    }
+
+    if length < len(requiredSets) {
+        return "", errors.New("password length too short for required character types")
+    }
+
     charset := lower + upper
     if useDigits {
         charset += digits
@@ -22,17 +54,31 @@ func Generate(length int, useDigits bool, useSymbols bool) (string, error) {
         charset += symbols
     }
 
-    if len(charset) == 0 {
-        return "", nil
-    }
-
     password := make([]byte, length)
-    for i := range password {
-        n, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+
+    // 必須カテゴリから1文字ずつ先に入れる
+    for i := 0; i < len(requiredSets); i++ {
+        c, err := randomCharFrom(requiredSets[i])
         if err != nil {
             return "", err
         }
-        password[i] = charset[n.Int64()]
+        password[i] = c
     }
-    return string(password), nil
+
+    // 残りの文字をランダムに埋める
+    for i := len(requiredSets); i < length; i++ {
+        c, err := randomCharFrom(charset)
+        if err != nil {
+            return "", err
+        }
+        password[i] = c
+    }
+
+    // シャッフルして順番をランダムに
+    shuffled, err := shuffle(password)
+    if err != nil {
+        return "", err
+    }
+
+    return string(shuffled), nil
 }
